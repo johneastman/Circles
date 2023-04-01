6c3768b99d18ce80c484d69b28d84aa95adc2018
John Eastman <jp.robotics@gmail.com>
2023-04-01 17:47:27 -0600

    attempt to fix null ref bug

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useRef } from "react";
 import "./Dialog.css";
 
@@ -7,33 +8,42 @@ interface DialogProps {
     negativeAction: () => void;
 }
 
-export function Dialog(props: DialogProps): JSX.Element {
+export class Dialog extends React.Component<DialogProps, {}> {
 
-    let dialogElement = useRef<HTMLDialogElement>(null);
+    dialogElement: React.RefObject<HTMLDialogElement>;
+    constructor(props: DialogProps) {
+        super(props);
 
-    if (props.isOpen) {
-        // Need to call "showModal" to make background inert (i.e, disable interaction with elements below dialog).
-        dialogElement.current!.showModal();
-    } else {
-        dialogElement.current!.close();
+        this.dialogElement = React.createRef<HTMLDialogElement>();
     }
 
-    return <dialog ref={dialogElement} className="dialog">
-        <div>
-            Are you sure you want to delete all your data? This will include:
-            <br/>
-            <ul>
-                <li>High Scores</li>
-                <li>Turret Mode</li>
-            </ul>
-            Once deleted, this data is gone forever and cannot be recovered.
+    componentDidUpdate(): void {
+        if (this.props.isOpen) {
+            // Need to call "showModal" to make background inert (i.e, disable interaction with elements below dialog).
+            this.dialogElement.current!.showModal();
+        } else {
+            this.dialogElement.current!.close();
+        }
+    }
 
-            <div className="horizontalList" style={{paddingTop: "1rem"}}>
+    render(): JSX.Element {
+        return <dialog ref={this.dialogElement} className="dialog">
+            <div>
+                Are you sure you want to delete all your data? This will include:
+                <br/>
                 <ul>
-                    <li style={{paddingRight: "0.5rem"}}><button className="button" onClick={props.positiveAction}>Yes, delete my data</button></li>
-                    <li style={{paddingLeft:  "0.5rem"}}><button className="button" onClick={props.negativeAction}>No, keep my data</button></li>
+                    <li>High Scores</li>
+                    <li>Turret Mode</li>
                 </ul>
+                Once deleted, this data is gone forever and cannot be recovered.
+
+                <div className="horizontalList" style={{paddingTop: "1rem"}}>
+                    <ul>
+                        <li style={{paddingRight: "0.5rem"}}><button className="button" onClick={this.props.positiveAction}>Yes, delete my data</button></li>
+                        <li style={{paddingLeft:  "0.5rem"}}><button className="button" onClick={this.props.negativeAction}>No, keep my data</button></li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </dialog>
+        </dialog>
+    }
 }
